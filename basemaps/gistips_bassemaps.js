8fad9fa4282d6e0dbf256172546c861beb7724b8
pjhooker <lima.cityplanner@gmail.com>
2016-11-12 22:47:53 +0100

    adding woodcut and spacestation

--- basemaps/gistips_bassemaps.js
+++ basemaps/gistips_bassemaps.js
@@ -15,4 +15,16 @@
   var tile_hot = 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
   var tile_hot_attr = 'attribution: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, '
     +'Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>';
-  
+
+//BASE MAPS by MapBox
+  var tile_woodcut = 'http://a.tiles.mapbox.com/v4/mapbox.b0v97egc/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpbG10dnA3NzY3OTZ0dmtwejN2ZnUycjYifQ.1W5oTOnWXQ9R1w8u3Oo1yA';
+  var tile_woodcut_attr = 'Map tiles by <a href="https://www.mapbox.com/">MapBox</a>, '
+    +'<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> <br> '
+    +'Map data: &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
+    +'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
+  var tile_spacestation = 'http://a.tiles.mapbox.com/v4/mapbox.4iecw76a/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpbG10dnA3NzY3OTZ0dmtwejN2ZnUycjYifQ.1W5oTOnWXQ9R1w8u3Oo1yA';
+  var tile_spacestation_attr = 'attribution: Map tiles by <a href="https://www.mapbox.com/">MapBox</a>, '
+    +'<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> <br>'
+    +'Map data: &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,'
+    +'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
+
