0dd7c198ad533faf285a4feaa31c37d19d86d234
pjhooker <lima.cityplanner@gmail.com>
2016-11-12 11:31:14 +0100

    Update gistips_bassemaps.js

--- basemaps/gistips_bassemaps.js
+++ basemaps/gistips_bassemaps.js
@@ -1,4 +1,4 @@
 var tile_carto_dark = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
-var tile_carto_dark_attr = 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)';
+var tile_carto_dark_attr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
 var tile_carto_light = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
-var tile_carto_light_attr = 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)';
+var tile_carto_light_attr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
