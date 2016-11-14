ee9d805cd1bfcecf441463110fce55d7daa74adf
pjhooker <lima.cityplanner@gmail.com>
2016-11-15 00:41:16 +0100

    Update gistips_bassemaps_list.js

--- basemaps/gistips_bassemaps_list.js
+++ basemaps/gistips_bassemaps_list.js
@@ -1,4 +1,9 @@
 var data = [
   {"Tile": "tile_carto_dark"},
-  {"Tile": "tile_carto_light"}
+  {"Tile": "tile_carto_light"},
+  {"Tile": "tile_opentopomap"},
+  {"Tile": "tile_hot"},
+  {"Tile": "tile_woodcut"},
+  {"Tile": "tile_spacestation"},
+  {"Tile": "tile_digitalglobe"}
 ];
