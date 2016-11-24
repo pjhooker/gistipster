8ea7c03f5e8d987ffbd0b446cafa7d9a25c763d7
pjhooker <lima.cityplanner@gmail.com>
2016-11-24 19:05:36 +0100

    Update gistips_bassemaps.js

--- basemaps/gistips_bassemaps.js
+++ basemaps/gistips_bassemaps.js
@@ -55,5 +55,5 @@
   //var tile_rl_dtm5x5_wms = 1;
   var tile_rl_dtm5x5_name = 'DTM 5X5';
   var tile_rl_dtm5x5 = 'http://www.cartografia.servizirl.it/arcgis/services/wms/DTM5_RL_wms/MapServer/WMSServer';
-  var tile_rl_dtm5x5_attr = ['<a href="https://www.mapbox.com/about/maps/" target="_blank">© RL © OpenStreetMap</a>',"wms"];
+  var tile_rl_dtm5x5_attr = ['wms','<a href="https://www.mapbox.com/about/maps/" target="_blank">© RL © OpenStreetMap</a>','image/jpeg',45.548586,9.299469,12];
 
