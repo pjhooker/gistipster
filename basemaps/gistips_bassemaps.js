63b75326c684e2a5bf97c8813b59c1487bf7b7b4
pjhooker <lima.cityplanner@gmail.com>
2016-11-24 23:24:03 +0100

    Update gistips_bassemaps.js

--- basemaps/gistips_bassemaps.js
+++ basemaps/gistips_bassemaps.js
@@ -1,7 +1,7 @@
 //BASE MAPS by carto.com >> https://carto.com/location-data-services/basemaps/
   var tile_carto_dark_name = 'CARTO dark';
   var tile_carto_dark = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
-  var tile_carto_dark_attr = ['provider','','','&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> '
+  var tile_carto_dark_attr = ['provider','World','osm,carto','&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> '
     +'&copy; <a href="http://cartodb.com/attributions">CartoDB</a>'];
 
   var tile_carto_light_name = 'CARTO light';
