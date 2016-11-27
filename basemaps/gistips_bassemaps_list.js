b770572c151e0f385e5e3d6a41bbb19415275427
pjhooker <lima.cityplanner@gmail.com>
2016-11-27 13:51:14 +0100

    Update gistips_bassemaps_list.js

--- basemaps/gistips_bassemaps_list.js
+++ basemaps/gistips_bassemaps_list.js
@@ -18,7 +18,7 @@ var data = [
   {"Tile": "tile_osm_nolabels"},
   {"Tile": "tile_ita_geo_100k"},
   {"Tile": "tile_aster_gdem_srtm"},
-  {"Tile": "tile_korona_adminb"}
+  {"Tile": "tile_korona_adminb"},
   {"Tile": "tile_cartodb_label"},
   {"Tile": "tile_stamen_labels_name"}
 ];
