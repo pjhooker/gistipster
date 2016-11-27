13c619086871521f7752d3beb3c34672b890f2ca
pjhooker <lima.cityplanner@gmail.com>
2016-11-27 01:50:54 +0100

    Update gistips_bassemaps.js

--- basemaps/gistips_bassemaps.js
+++ basemaps/gistips_bassemaps.js
@@ -67,7 +67,7 @@
   +'Geoportale della Lombardia - CC-BY-NC-SA 3.0 Italia</a>','0','image/jpeg',45.465361, 9.183910,17];
 
  var tile_simpson_name = 'Simpson\'s City';
- var tile_simpson = 'http://cityplanner/webapp/springfieldmap/{z}/{x}/{y}.png';
+ var tile_simpson = 'http://cityplanner.it/webapp/springfieldmap/{z}/{x}/{y}.png';
  var tile_simpson_attr = ['wms','Simpson','PixelArt',                
   + 'Imagery © <a href="http://www.smaxity.net/images/SpringfieldMap.jpg">smaxity.net/</a>'
   + '<br>Map data <a href="http://simpsons.playgis.com/">simpsons.playgis</a>'
